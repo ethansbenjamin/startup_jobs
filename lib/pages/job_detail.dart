25befe28b72f13a9ecb86e81b588721cbc242edb
Ethan Benjamin <ethanb3njamin@gmail.comn>
2020-10-27 01:22:25 -0400

    added a url launcher for applying to jobs

--- lib/pages/job_detail.dart
+++ lib/pages/job_detail.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_graphql_bloc/api/models/models.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class JobDetail extends StatelessWidget {
   static Route route() {
@@ -10,6 +11,9 @@ class JobDetail extends StatelessWidget {
   const JobDetail({this.job});
 
   final Job job;
+
+  void _launchURL() async {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,16 +21,37 @@ class JobDetail extends StatelessWidget {
         title: Text(job.title ?? ""),
       ),
       body: Container(
-        width: MediaQuery.of(context).size.width * .9,
-        child: ListView(
+          child: SingleChildScrollView(
+        child: Column(
           children: [
-            Text(job.title ?? ""),
-            Text(job.locationNames ?? ""),
+            Text(
+              job.title ?? "",
+              style: TextStyle(fontSize: 20),
+            ),
+            RaisedButton(
+              onPressed: () async {
+                String url = job.applyUrl;
+                if (await canLaunch(url)) {
+                  await launch(
+                    url,
+                    forceWebView: true,
+                    forceSafariVC: true,
+                  );
+                } else {
+                  throw 'Could not launch $url';
+                }
+              },
+              child: Text("Apply"),
+            ),
+            Text(
+              job.locationNames ?? "",
+              style: TextStyle(fontSize: 20),
+            ),
             Text(job.applyUrl ?? ""),
             MarkdownBody(data: job.description ?? ""),
           ],
         ),
-      ),
+      )),
     );
   }
 }
