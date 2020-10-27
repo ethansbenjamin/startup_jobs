01b33841b5e934e67b2c8d37e52e0c7888385c40
Ethan Benjamin <ethanb3njamin@gmail.comn>
2020-10-26 23:16:42 -0400

    added our json annotations and job model

---
+++ lib/api/models/job.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'job.g.dart';
+
+@JsonSerializable(createToJson: false)
+class Job {
+  const Job({
+    @required this.id,
+    @required this.title,
+    @required this.locationNames,
+    @required this.description,
+    this.isFeatured = false,
+  });
+
+  factory Job.fromJson(Map<String, dynamic> json) => _$JobFromJson(json);
+
+  final String id;
+  final String title;
+  final String locationNames;
+  final bool isFeatured;
+  final String description;
+}
